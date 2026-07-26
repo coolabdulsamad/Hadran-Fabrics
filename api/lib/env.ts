c56f509b3c25696ada4d296110f1d49afe95fb92
coolabdulsamad <coolabdulsamad09@gmail.com>
2026-07-26 06:18:11 +0100

    Resolve merge conflict in env.ts

--- api/lib/env.ts
+++ api/lib/env.ts
@@ -9,11 +9,6 @@ function required(name: string): string {
 }
 
 export const env = {
-<<<<<<< HEAD
-  appId: required("APP_ID"),
-  appSecret: required("APP_SECRET"),
-=======
->>>>>>> 0b9da5b (Remove unused APP_ID/APP_SECRET env requirements)
   isProduction: process.env.NODE_ENV === "production",
   databaseUrl: required("DATABASE_URL"),
 };
